7d62dfdaf618acf6432c6411acea0fffc565f0bc
jeongbin <qls2467@gmail.com>
2022-07-20 11:13:29 -0600

    To organize using functions

--- main.js
+++ main.js
@@ -2,6 +2,32 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
+const templateHTML = (title, list, body) => {
+  return `
+<!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  ${list}
+  ${body}
+</body>
+</html>
+`;
+};
+const templateList = (fillist) => {
+  let list = "<ul>";
+  let i = 0;
+  while (i < fillist.length) {
+    list = list + `<li><a href="/?id=${fillist[i]}">${fillist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + "</ul>";
+  return list;
+};
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -15,65 +41,28 @@ var app = http.createServer(function (request, response) {
 
         var title = "welcome";
         var description = "hello node.js";
-        let list = "<ul>";
-        let i = 0;
-        while (i < fillist.length) {
-          list =
-            list + `<li><a href="/?id=${fillist[i]}">${fillist[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + "</ul>";
-        var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+        var list = templateList(fillist);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readdir("./data", (error, fillist) => {
-        //console.log(fillist); // [ 'CSS', 'HTML', 'JavaScript' ]
-        var title = "welcome";
-        var description = "hello node.js";
-        let list = "<ul>";
-        let i = 0;
-        while (i < fillist.length) {
-          list =
-            list + `<li><a href="/?id=${fillist[i]}">${fillist[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + "</ul>";
         fs.readFile(
           `data/${queryData.id}`,
           "utf8",
           function (err, description) {
             var title = queryData.id;
-            var template = `
-              <!doctype html>
-              <html>
-              <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-              </body>
-              </html>
-              `;
+            var list = templateList(fillist);
+            var template = templateHTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description}`
+            );
             response.writeHead(200);
             response.end(template);
           }
