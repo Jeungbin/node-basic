5851f84ec2fb3f1496337e4afabebf17b26bde39
jeongbin <qls2467@gmail.com>
2022-07-20 11:25:13 -0600

    동기 비동기

---
+++ syntax/sync.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+/*
+//readFileSync
+console.log("A");
+const result = fs.readFileSync("syntax/sample.txt", "utf-8");
+// 'utf-8' 사람이 읽을수 있는 형식으로 변환
+console.log(result);
+console.log("c");
+*/
+
+// 비동기
+console.log("A");
+fs.readFile("syntax/sample.txt", "utf-8", (err, result) => {
+  console.log(result);
+});
+console.log("c");
